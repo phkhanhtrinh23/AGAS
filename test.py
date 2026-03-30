ddfed36e76e79e2af842166b7f155bd5d889ed03
phkhanhtrinh23 <phkhanhtrinh23@gmail.com>
2026-03-30 13:34:00 +1000

    update code

--- test.py
+++
@@ -1,6 +0,0 @@
-import json
-obj=json.load(open("outputs/episode_lockout_example.json"))
-for step in obj["history"]:
-    rt = step.get("coordinator_runtime_trace")
-    if rt and rt.get("sniper_lockouts"):
-        print(step["step"], rt)
