3cb98ab3764f3de84ed0a817738c68ca1cd623e0
phkhanhtrinh23 <phkhanhtrinh23@gmail.com>
2026-03-31 10:58:09 +1000

    update code

--- run_test_black_box.sh
+++ run_test_black_box.sh
@@ -46,25 +46,46 @@ set -euo pipefail
 #   --output outputs/transfer_top50_implicit.json \
 #   --transfer-candidate-set all_items
 
-  PYTHONPATH=src python -m agas.cli run-transfer \
+  # PYTHONPATH=src python -m agas.cli run-transfer \
+  # --processed-root processed \
+  # --dataset ml-latest-small \
+  # --max-interactions 5000 \
+  # --n-factors 24 \
+  # --target-item-id 1215  \
+  # --target-keyword horror \
+  # --goal-rank 3 \
+  # --num-steps 100 \
+  # --num-agents 4 \
+  # --transfer-mode option-a \
+  # --target-models neumf,lightgcn \
+  # --target-epochs 3 \
+  # --target-batch-size 2048 \
+  # --coordinator-policy openai \
+  # --worker-policy openai \
+  # --llm-model gpt-5-mini \
+  # --openai-api-key "${OPENAI_API_KEY}" \
+  # --prompt-root prompts \
+  # --stop-on-goal \
+  # --no-target-implicit-only \
+  # --episode-model lightgcn \
+  # --output outputs/transfer_top50_no_implicit_lightgcn.json
+
+PYTHONPATH=src python -m agas.cli run-transfer \
   --processed-root processed \
+  --max-interactions 5000 \
   --dataset ml-latest-small \
-  --max-interactions 10000 \
   --n-factors 24 \
-  --target-item-id 1215  \
+  --target-item-id 1215 \
   --target-keyword horror \
+  --num-steps 20 \
   --goal-rank 3 \
-  --num-steps 100 \
   --num-agents 4 \
-  --transfer-mode option-a \
-  --target-models neumf,lightgcn \
-  --target-epochs 3 \
-  --target-batch-size 2048 \
-  --coordinator-policy openai \
+  --use-segment-users-as-agents \
+  --transfer-attack-roles sniper \
+  --no-target-implicit-only \
   --worker-policy openai \
   --llm-model gpt-5-mini \
   --openai-api-key "${OPENAI_API_KEY}" \
-  --prompt-root prompts \
-  --stop-on-goal \
-  --no-target-implicit-only \
-  --output outputs/transfer_top50_no_implicit.json
+  --transfer-candidate-set cluster \
+  --episode-model lightgcn \
+  --output outputs/transfer_fixed_no_target_implicit_lightgcn.json
